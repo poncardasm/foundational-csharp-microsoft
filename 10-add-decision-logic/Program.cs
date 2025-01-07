5718f31e88695db1269b98b87353b188a04e5807
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-08 01:13:42 +0200

    Fix decision logic for doubles and triples in dice game

--- 10-add-decision-logic/Program.cs
+++ 10-add-decision-logic/Program.cs
@@ -12,19 +12,17 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1}, {roll2}, {roll3} = {total}");
 
-
-
-if ((roll1 == roll2) && (roll2 == roll3))
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
 {
-  if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
-  {
-  total += 2;
-  Console.WriteLine($"You rolled doubles! +2 bonus to your total = {total}!");
-  }
-  else 
+  if ((roll1 == roll2) && (roll2 == roll3))
   {
     total += 6;
     Console.WriteLine($"You rolled triples! +6 bonus to your total = {total}!");
+  }
+  else 
+  {
+    total += 2;
+    Console.WriteLine($"You rolled doubles! +2 bonus to your total = {total}!");
   }
 }
 

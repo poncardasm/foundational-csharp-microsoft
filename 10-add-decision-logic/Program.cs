1277e40437bb6a3110892ae92e3dd9e2903c88bf
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-08 00:54:29 +0200

    Add bonus point logic and win condition comments for dice game

--- 10-add-decision-logic/Program.cs
+++ 10-add-decision-logic/Program.cs
@@ -1,4 +1,8 @@
-﻿Random dice = new Random();
+﻿// If any two dice you roll result in the same value, you get two bonus points for rolling doubles.
+// If all three dice you roll result in the same value, you get six bonus points for rolling triples.
+// If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
+
+Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);

9fa536f5e15c48d931f0a4568a0ba8de091c7ed6
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-09 12:54:47 +0200

    Update foreach implementation to report inventory items with running total

--- 11-1_implement-foreach/Program.cs
+++ 11-1_implement-foreach/Program.cs
@@ -4,8 +4,14 @@ Exercise - Implement the foreach statement
 Suppose you work for a manufacturing company. The company needs you to complete an inventory of your warehouse to determine the number of products that are ready to ship. In addition to the total number of finished products, you need to report the number of finished products stored in each individual bin in your warehouse, along with a running total. This running total will be used to create an audit trail so you can double-check your work and identify "shrinkage".
 */
 
-string[] names = [ "Rowena", "Robin", "Bao" ];
-foreach (string name in names)
+// string[] names = [ "Rowena", "Robin", "Bao" ];
+// foreach (string name in names)
+// {
+//   Console.WriteLine(name);
+// }
+
+int[] inventory = [ 200, 450, 700, 175, 250 ];
+foreach (int items in inventory)
 {
-  Console.WriteLine(name);
+  Console.WriteLine(items);
 }

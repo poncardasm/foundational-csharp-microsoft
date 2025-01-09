94dee23ee5297da97fa15eecfc91980ba31500af
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-09 12:51:24 +0200

    Implement foreach statement for inventory reporting with example output

--- 11-1_implement-foreach/Program.cs
+++ 11-1_implement-foreach/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Exercise - Implement the foreach statement
+
+Suppose you work for a manufacturing company. The company needs you to complete an inventory of your warehouse to determine the number of products that are ready to ship. In addition to the total number of finished products, you need to report the number of finished products stored in each individual bin in your warehouse, along with a running total. This running total will be used to create an audit trail so you can double-check your work and identify "shrinkage".
+*/
+
+string[] names = [ "Rowena", "Robin", "Bao" ];
+foreach (string name in names)
+{
+  Console.WriteLine(name);
+}

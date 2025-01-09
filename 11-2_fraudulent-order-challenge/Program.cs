20be166ea2b6282f10b0d2ef3f718f69c022026d
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-09 13:16:39 +0200

    Implement order ID reporting for potential fraud detection

--- 11-2_fraudulent-order-challenge/Program.cs
+++ 11-2_fraudulent-order-challenge/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Code challenge - Report the Order IDs that need further investigation
+
+Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the fraud team to investigate further.
+*/
+
+string[] fraudulentOrderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
+
+Console.WriteLine("Order IDs: ");
+foreach (string IDs in fraudulentOrderIDs)
+{
+  Console.WriteLine(IDs);
+
+  if(IDs.StartsWith("B"))
+  {
+    Console.WriteLine($"Potentially fraudulent Order ID: {IDs}");
+  }
+}
+
+
+
+
+
+
+
+

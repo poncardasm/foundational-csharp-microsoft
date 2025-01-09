2aead8f34b495db97b4d66a8dd8bed230a0195d9
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-09 12:37:29 +0200

    Refactor fraudulent order IDs initialization and output for clarity

--- 11_array-basics/Program.cs
+++ 11_array-basics/Program.cs
@@ -1,9 +1,18 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿// string[] fraudulentOrderIDs = new string[3];
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign first: {fraudulentOrderIDs[0]}");

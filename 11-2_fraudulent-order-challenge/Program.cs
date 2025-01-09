48635eb64bb066fd84d65c9410c1f4e317516bda
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-09 13:19:31 +0200

    Refactor order ID reporting to identify potential fraud by checking for IDs starting with 'B'

--- 11-2_fraudulent-order-challenge/Program.cs
+++ 11-2_fraudulent-order-challenge/Program.cs
@@ -4,16 +4,22 @@ Code challenge - Report the Order IDs that need further investigation
 Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the fraud team to investigate further.
 */
 
-string[] fraudulentOrderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
+string[] OrderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
 
 Console.WriteLine("Order IDs: ");
-foreach (string IDs in fraudulentOrderIDs)
+foreach (string IDs in OrderIDs)
 {
   Console.WriteLine(IDs);
 
-  if(IDs.StartsWith("B"))
+  
+}
+
+Console.WriteLine("\nPotential fraudulent Order ID:");
+foreach (string IDs in OrderIDs)
+{
+  if(IDs.StartsWith('B'))
   {
-    Console.WriteLine($"Potentially fraudulent Order ID: {IDs}");
+    Console.WriteLine($"{IDs}");
   }
 }
 

0c460db2a30f522666a5ec3be8a417cd011e0fd7
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-11 22:13:07 +0200

    Implement string reversal and count occurrences of 'o' in the message

--- 12-1_improve-code-readability/Program.cs
+++ 12-1_improve-code-readability/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// Convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the char
+Array.Reverse(charMessage);
+int x = 0;
+// Count the o's
+foreach (char i in charMessage)
+{
+  if (i == 'o')
+  {
+    x++;
+  }
+}
+// Convert back to a string
+string new_message = new string(charMessage);
+// Print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");

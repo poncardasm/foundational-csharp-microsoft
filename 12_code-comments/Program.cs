b528facc2f75d02abf481503c04d3aadbfc9edf6
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-11 21:56:33 +0200

    Generate random order IDs and display them in the console

--- 12_code-comments/Program.cs
+++ 12_code-comments/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+  int prefixValue = random.Next(65, 70);
+  string prefix = Convert.ToChar(prefixValue).ToString();
+  string suffix = random.Next(1, 1000).ToString("0000");
+
+  orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+  Console.WriteLine(orderID);
+}

f4d412c053877642752610309de0442fd675c9de
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-12 17:05:08 +0200

    Refactor Sophia's score calculation to improve scope and readability

--- 13_student-grading-app/Program.cs
+++ 13_student-grading-app/Program.cs
@@ -2,26 +2,28 @@
 int currentAssignments = 5;
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 // Student names
 string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
 
-int sophiaSum = 0;
 
-decimal sophiaScoreAvg;
+
 
 foreach (string name in studentNames)
 {
   Console.WriteLine($"{name}");
+  int sophiaSum = 0;
+  
+  decimal sophiaScoreAvg;
+
+  foreach (int score in sophiaScores)
+  {
+    // Add the exam score to the sum
+    sophiaSum += score;
+  }
 }
 
-foreach (int score in sophiaScores)
-{
-  sophiaSum += score;
-}
+
 
 sophiaScoreAvg = (decimal)sophiaSum / currentAssignments;
 

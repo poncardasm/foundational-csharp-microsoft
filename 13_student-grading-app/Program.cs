54d0c0297a09b4040b0bf0dfc8d43e6258cbc64d
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-12 17:08:25 +0200

    Refactor Sophia's score calculation logic for improved clarity and organization

--- 13_student-grading-app/Program.cs
+++ 13_student-grading-app/Program.cs
@@ -4,31 +4,31 @@ int currentAssignments = 5;
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 
 // Student names
-string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 
 
 
 foreach (string name in studentNames)
 {
-  Console.WriteLine($"{name}");
-  int sophiaSum = 0;
-  
-  decimal sophiaScoreAvg;
-
-  foreach (int score in sophiaScores)
+  if (name == "Sophia")
   {
-    // Add the exam score to the sum
-    sophiaSum += score;
+    int sophiaSum = 0;
+
+    decimal sophiaScoreAvg;
+
+    foreach (int score in sophiaScores)
+    {
+      // Add the exam score to the sum
+      sophiaSum += score;
+    }
+
+    sophiaScoreAvg = (decimal)sophiaSum / currentAssignments;
+
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScoreAvg + "\tA-");
   }
 }
-
-
-
-sophiaScoreAvg = (decimal)sophiaSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScoreAvg + "\tA-");
 
 Console.WriteLine("\nPress the Enter key to continue");
 Console.ReadLine();

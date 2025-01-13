d6960f6c7d04355d81093917cf12cd2c6295ab1b
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-13 23:29:27 +0200

    Add boolean expression evaluations to the console output

--- 14_evaluate-boolean-expressions/Program.cs
+++ 14_evaluate-boolean-expressions/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");

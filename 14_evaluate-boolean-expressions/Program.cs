de6f8f5e8715102428bad5d1319f6b546ccdaf59
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-13 23:34:50 +0200

    Add inequality operator evaluations to boolean expression output

--- 14_evaluate-boolean-expressions/Program.cs
+++ 14_evaluate-boolean-expressions/Program.cs
@@ -8,3 +8,12 @@ Console.WriteLine(myValue == "a");
 string value1 = " a";
 string value2 = "A ";
 Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+Console.WriteLine("Use the inequality operator");
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine("1" != "2");
+
+string myValue2 = "a";
+Console.WriteLine(myValue2 != "a");

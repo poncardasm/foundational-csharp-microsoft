daf0ccf68febbad45bb62bdae5971175500ffc00
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-15 01:44:35 +0200

    Refactor discount calculation to streamline output

--- 14-1_conditional-operator/Program.cs
+++ 14-1_conditional-operator/Program.cs
@@ -1,3 +1,7 @@
 ﻿int saleAmount = 1001;
 int discount = saleAmount > 100 ? 100 : 50;
 Console.WriteLine($"Discount: {discount}");
+
+// v2
+Console.WriteLine($"Discount: {(saleAmount > 100 ? 100 : 50)}");
+

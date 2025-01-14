318417209bb4ad6a3bad6501a86cf0d96f218ccc
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-14 17:40:47 +0200

    Add comparison operator evaluations to boolean expression output

--- 14_evaluate-boolean-expressions/Program.cs
+++ 14_evaluate-boolean-expressions/Program.cs
@@ -9,7 +9,7 @@ string value1 = " a";
 string value2 = "A ";
 Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-Console.WriteLine("Use the inequality operator");
+Console.WriteLine("\nInequality operator:\n");
 
 Console.WriteLine("a" != "a");
 Console.WriteLine("a" != "A");
@@ -17,3 +17,10 @@ Console.WriteLine("1" != "2");
 
 string myValue2 = "a";
 Console.WriteLine(myValue2 != "a");
+
+Console.WriteLine("\nComparison Operators:\n");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);

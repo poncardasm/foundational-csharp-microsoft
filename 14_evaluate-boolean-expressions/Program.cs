d477e33b6a03b44481f3e1cca0ceb6e87d041a65
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-14 17:46:35 +0200

    Add comparison operator evaluations for integers and strings

--- 14_evaluate-boolean-expressions/Program.cs
+++ 14_evaluate-boolean-expressions/Program.cs
@@ -39,3 +39,12 @@ Console.WriteLine(!pangram.Contains("fox"));
 Console.WriteLine(!pangram.Contains("fox"));
 Console.WriteLine(!pangram.Contains("cow"));
 
+Console.WriteLine("\nComparison Operators:\n");
+
+int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); // output: False
+

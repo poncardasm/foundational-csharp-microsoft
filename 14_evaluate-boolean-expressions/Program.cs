b837977e1de9e47a5ab46ab9ac53542d5c078869
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-14 17:46:02 +0200

    Add additional boolean evaluations for string containment checks

--- 14_evaluate-boolean-expressions/Program.cs
+++ 14_evaluate-boolean-expressions/Program.cs
@@ -24,3 +24,18 @@ Console.WriteLine(1 > 2);
 Console.WriteLine(1 < 2);
 Console.WriteLine(1 >= 1);
 Console.WriteLine(1 <= 1);
+
+Console.WriteLine("\nComparison Operators:\n");
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
+

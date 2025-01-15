3ad6f3685b08374f8b8aac7a8959797f04a066bd
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-15 15:05:14 +0200

    Add description for decision logic challenge implementation

--- 14-3_challenge-decision-logic/Program.cs
+++ 14-3_challenge-decision-logic/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+  # Decision logic challenge
+  
+  In this challenge, you'll implement decision logic based on a series of business rules. The business rules specify the access that will be granted to users based on their role-based permissions and their career level. 
+  
+  Code branches will display a different message to the user depending on their permissions and level.
+*/

a78c66ceca13c7d945d6e50422401c7827032e65
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-15 14:50:32 +0200

    Add description for conditional operator coin flip challenge

--- 14-2_challenge-coin-flip/Program.cs
+++ 14-2_challenge-coin-flip/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+  Conditional operator challenge
+  In this challenge, you'll implement a conditional operator to simulate a "coin flip". The resulting decision logic will display either heads or tails.
+
+  - Use the Random class to generate a value.
+  - Consider the range of numbers that is required.
+  - Based on the value generated, use the conditional operator to display either heads or    tails.
+  - There should be a 50% chance that the result is either heads or tails.
+  - Your code should be easy to read, but with as few lines as possible.
+  - You should be able to accomplish the desired result in three lines of code.
+*/
+

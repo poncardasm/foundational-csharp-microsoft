15a19e4e5c7330cbc75805eb829e08a2fed59369
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-17 13:11:20 +0200

    Refactor code block examples to enhance clarity and demonstrate variable scope

--- 15_code-block-variable-scope/Program.cs
+++ 15_code-block-variable-scope/Program.cs
@@ -1,14 +1,17 @@
-﻿// Create a variable inside of a code block
-bool flag = true;
-if (flag)
-{
-  int value = 10;
-  Console.WriteLine($"Inside the code block: {value}");
-}
+﻿/*********************************************
+  Create a variable inside of a code block
+*********************************************/
 
-/*
+// bool flag = true;
+// if (flag)
+// {
+//   int value = 10;
+//   Console.WriteLine($"Inside the code block: {value}");
+// }
+
+/*********************************************
   Attempt to access a variable outside the code block in which it's declared
-*/
+*********************************************/
 
 // bool flag2 = true;
 // if (flag2)
@@ -18,16 +21,46 @@ if (flag)
 // }
 // Console.WriteLine($"Inside the code block: {value2}");
 
-/*
+/*********************************************
   Move the variable declaration above the code block
-*/
-bool flag3 = true;
-int value3;
+*********************************************/
 
-if (flag3)
+// bool flag3 = true;
+// int value3;
+
+// if (flag3)
+// {
+//   Console.WriteLine($"Inside the code block: {value3}");
+// }
+
+// value3 = 10;
+// Console.WriteLine($"Outside the code block: {value3}");
+
+/*********************************************
+  Initialize a variable as part of variable declaration
+*********************************************/
+
+// bool flag4 = true;
+// int value4 = 0;
+
+// if (flag4)
+// {
+//   Console.WriteLine($"Inside the code block: {value4}");
+// }
+
+// value4 = 10;
+// Console.WriteLine($"Outside the code block: {value4}");
+
+/*********************************************
+  Examine the compiler's interpretation of your code
+*********************************************/
+
+int value;
+
+if (true)
 {
-  Console.WriteLine($"Inside the code block: {value3}");
+  value = 10;
+  Console.WriteLine($"Inside the code block: {value}");
 }
 
-value3 = 10;
-Console.WriteLine($"Outside the code block: {value3}");
+Console.WriteLine($"Outside the code block: {value}");

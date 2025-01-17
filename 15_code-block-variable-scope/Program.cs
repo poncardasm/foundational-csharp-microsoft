91abeb21d75e61f191b7b8f1f7370955c66deec4
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-17 12:59:23 +0200

    Add demonstration of variable scope by accessing a variable outside its declaration block

--- 15_code-block-variable-scope/Program.cs
+++ 15_code-block-variable-scope/Program.cs
@@ -5,3 +5,13 @@ if (flag)
   int value = 10;
   Console.WriteLine($"Inside the code block: {value}");
 }
+
+// Attempt to access a variable outside the code block in which it's declared
+
+bool flag2 = true;
+if (flag2)
+{
+  int value2 = 10;
+  Console.WriteLine($"Inside the code block: {value2}");
+}
+Console.WriteLine($"Inside the code block: {value2}");

45088d449fb5954203ac505ab9f0ba2cc4bdcc74
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-17 12:57:11 +0200

    Implement variable declaration within a code block and output its value

--- 15_code-block-variable-scope/Program.cs
+++ 15_code-block-variable-scope/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a variable inside of a code block
+bool flag = true;
+if (flag)
+{
+  int value = 10;
+  Console.WriteLine($"Inside the code block: {value}");
+}

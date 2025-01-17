241bc35be5446a235b62f2b009add06d288bbf09
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-17 13:03:40 +0200

    Refactor variable scope demonstration to show declaration outside code block and access from within

--- 15_code-block-variable-scope/Program.cs
+++ 15_code-block-variable-scope/Program.cs
@@ -6,12 +6,28 @@ if (flag)
   Console.WriteLine($"Inside the code block: {value}");
 }
 
-// Attempt to access a variable outside the code block in which it's declared
+/*
+  Attempt to access a variable outside the code block in which it's declared
+*/
 
-bool flag2 = true;
-if (flag2)
+// bool flag2 = true;
+// if (flag2)
+// {
+//   int value2 = 10;
+//   Console.WriteLine($"Inside the code block: {value2}");
+// }
+// Console.WriteLine($"Inside the code block: {value2}");
+
+/*
+  Move the variable declaration above the code block
+*/
+bool flag3 = true;
+int value3;
+
+if (flag3)
 {
-  int value2 = 10;
-  Console.WriteLine($"Inside the code block: {value2}");
+  Console.WriteLine($"Inside the code block: {value3}");
 }
-Console.WriteLine($"Inside the code block: {value2}");
+
+value3 = 10;
+Console.WriteLine($"Outside the code block: {value3}");

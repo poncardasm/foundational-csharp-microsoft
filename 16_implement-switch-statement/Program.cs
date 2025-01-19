4ed2d563fa0687981b095d139ad0d2f9ec60379f
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-19 20:34:57 +0200

    Implement switch statement for employee title assignment based on level

--- 16_implement-switch-statement/Program.cs
+++ 16_implement-switch-statement/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+  case 100:
+    title = "Junior Associate";
+    break;
+  case 200:
+    title = "Senior Associate";
+    break;
+  case 300:
+    title = "Manager";
+    break;
+  case 400:
+    title = "Senior Manager";
+    break;
+  default:
+    title = "Associate";
+    break;
+}
+
+Console.WriteLine($"{employeeName}, {employeeLevel}");

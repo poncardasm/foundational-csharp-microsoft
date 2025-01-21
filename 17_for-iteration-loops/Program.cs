26a17cc6baa491a42f8ab2412cb48244b7e193ad
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-22 01:10:33 +0200

    Update for loop to iterate from 10 to 0 and add comments for clarity

--- 17_for-iteration-loops/Program.cs
+++ 17_for-iteration-loops/Program.cs
@@ -1,4 +1,17 @@
-﻿for (int i = 0; i < 10; i++)
+﻿/*
+  Write a basic for statement
+*/
+
+// for (int i = 0; i < 10; i++)
+// {
+//   Console.WriteLine(i);
+// }
+
+/*
+  Change the iteration conditions
+*/
+
+for (int i = 10; i >= 0; i--)
 {
   Console.WriteLine(i);
 }

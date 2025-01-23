9a739ea729d0d0f20d07aacb7cbbab661045ecc4
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-23 20:06:42 +0200

    Add comments to explain the use of do-while loop in random number generation

--- 18_do-while/Program.cs
+++ 18_do-while/Program.cs
@@ -1,4 +1,9 @@
-﻿Random randomNum = new Random();
+﻿/*
+  Add looping logic to your code using the do-while and while statements in C#
+  https://learn.microsoft.com/en-us/training/modules/csharp-do-while/
+*/
+
+Random randomNum = new Random();
 int current = 0;
 
 do

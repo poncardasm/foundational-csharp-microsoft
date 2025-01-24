3411666f5ac4539443e5f784e3f359a4dc9a9c2f
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-25 00:32:03 +0200

    Add placeholder messages for menu options in Contoso Pets application

--- 19-0_contoso-pets-application/Program.cs
+++ 19-0_contoso-pets-application/Program.cs
@@ -82,6 +82,7 @@ do
   Console.Clear();
 
   Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:\n");
+
   Console.WriteLine(" 1. List all of our current pet information");
   Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
   Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
@@ -90,7 +91,9 @@ do
   Console.WriteLine(" 6. Edit an animal's personality description");
   Console.WriteLine(" 7. Display all cats with a specified characteristic");
   Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+
   Console.WriteLine();
+
   Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
   readResult = Console.ReadLine();
@@ -108,21 +111,61 @@ do
   switch (menuSelection)
   {
     case "1":
+      // List all of our current pet information
+      Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "2":
+      // Add a new animal friend to the ourAnimals array
+      Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "3":
+      // Ensure animal ages and physical descriptions are complete
+      Console.WriteLine("Challenge Project - please check back soon to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "4":
+      // Ensure animal nicknames and personality descriptions are complete
+      Console.WriteLine("Challenge Project - please check back soon to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "5":
+      // Edit an animal's age
+      Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "6":
+      // Edit an animal's personality description
+      Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "7":
+      // Display all cats with a specified characteristic
+      Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     case "8":
+      // Display all dogs with a specified characteristic
+      Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+      Console.WriteLine("Press the Enter key to continue.");
+      readResult = Console.ReadLine();
       break;
+
     default:
       break;
   }

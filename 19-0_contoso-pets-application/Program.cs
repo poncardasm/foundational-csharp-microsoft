f6ea3d4b8a35de31e29f49f5f8cc0d9be3310ba2
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-01-25 00:16:32 +0200

    Refactor main menu display logic for improved readability and user interaction

--- 19-0_contoso-pets-application/Program.cs
+++ 19-0_contoso-pets-application/Program.cs
@@ -75,30 +75,33 @@ for (int i = 0; i < maxPets; i++)
   ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 }
 
-// DIsplay the top-level menu options
-
-Console.Clear();
-
-Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:\n");
-Console.WriteLine(" 1. List all of our current pet information");
-Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-Console.WriteLine(" 5. Edit an animal's age");
-Console.WriteLine(" 6. Edit an animal's personality description");
-Console.WriteLine(" 7. Display all cats with a specified characteristic");
-Console.WriteLine(" 8. Display all dogs with a specified characteristic");
-Console.WriteLine();
-Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
-
-readResult = Console.ReadLine();
-if (readResult != null)
+do
 {
-  menuSelection = readResult.ToLower();
-}
+  // DIsplay the top-level menu options
 
-Console.WriteLine($"You selected menu option {menuSelection}");
-Console.WriteLine("Press 'Enter' key to continue");
+  Console.Clear();
 
-// Pause code execution
-readResult = Console.ReadLine();
+  Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:\n");
+  Console.WriteLine(" 1. List all of our current pet information");
+  Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+  Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+  Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+  Console.WriteLine(" 5. Edit an animal's age");
+  Console.WriteLine(" 6. Edit an animal's personality description");
+  Console.WriteLine(" 7. Display all cats with a specified characteristic");
+  Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+  Console.WriteLine();
+  Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+  readResult = Console.ReadLine();
+  if (readResult != null)
+  {
+    menuSelection = readResult.ToLower();
+  }
+
+  Console.WriteLine($"You selected menu option {menuSelection}");
+  Console.WriteLine("Press 'Enter' key to continue");
+
+  // Pause code execution
+  readResult = Console.ReadLine();
+} while (menuSelection != "exit");

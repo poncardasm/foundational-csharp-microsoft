d314ad6bc7448a7d978c0f113c5e1098f5100935
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-01 02:29:32 +0200

    Enhance integral types demonstration by adding unsigned integral types output

--- 21-0_integral-types/Program.cs
+++ 21-0_integral-types/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Signed Integral types:");
+﻿Console.WriteLine("Signed Integral types:\n");
 
 Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
 Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
 Console.WriteLine($"int : {int.MinValue} to {int.MaxValue}");
 Console.WriteLine($"long : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("\nUnsigned Integral types:\n");
+
+Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong : {ulong.MinValue} to {ulong.MaxValue}");

97cc3f466e2862aa58a6717dbb1f4411554e23e2
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-02 22:53:38 +0200

    Update data type casting demonstration to include new examples and comment out previous code

--- 22-0_data-type-casting-conversion/Program.cs
+++ 22-0_data-type-casting-conversion/Program.cs
@@ -1,5 +1,11 @@
-﻿int first = 2;
-string second = "4";
-string result = first + second;
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
+
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine(myDecimal);

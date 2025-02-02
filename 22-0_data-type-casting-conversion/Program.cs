61914a7c1cfd035c140cfcbc911762673b769780
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-02 23:16:25 +0200

    Enhance data type casting demonstration with improved comments and additional examples

--- 22-0_data-type-casting-conversion/Program.cs
+++ 22-0_data-type-casting-conversion/Program.cs
@@ -10,12 +10,31 @@
 // decimal myDecimal = myInt;
 // Console.WriteLine(myDecimal);
 
-/* 
-  PERFORM A CAST
-*/
+/****
+  Perform a cast
+***/
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"Decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"Decimal: {myDecimal}");
 
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+/***
+  Determine if your conversion is a "widening conversion" or a "narrowing conversion"
+***/
+
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+/***
+  Use ToString() to convert a number to a string
+***/
+
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);

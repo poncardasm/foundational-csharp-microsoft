ad1375960b1e60c5da784174362c03d9295323e0
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-04 01:01:40 +0200

    Add TryParse method demonstration project with example implementation

---
+++ 22-1_tryparse-method/Program.cs
@@ -0,0 +1,11 @@
+﻿string value = "102";
+int result = 0;
+
+if (int.TryParse(value, out result))
+{
+  Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+  Console.WriteLine($"Unable to report the measurement.")
+}

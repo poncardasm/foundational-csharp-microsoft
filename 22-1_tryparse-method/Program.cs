f15f1557ec3549584e5d5b8f6c4fa23a22390f86
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-04 01:11:24 +0200

    Refactor TryParse method demonstration to improve clarity and handle parsing errors more effectively

--- 22-1_tryparse-method/Program.cs
+++ 22-1_tryparse-method/Program.cs
@@ -1,13 +1,32 @@
-﻿string value = "102";
+﻿// string value = "102";
+// int result = 0;
+
+// if (int.TryParse(value, out result))
+// {
+//   Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//   Console.WriteLine($"Unable to report the measurement.");
+// }
+
+// Console.WriteLine($"Measurement with offset: {50 + result}");
+
+/*
+  Modify the string variable to a value that can't be parsed
+*/
+
+string value = "bad";
 int result = 0;
 
 if (int.TryParse(value, out result))
 {
-  Console.WriteLine($"Measurement: {result}");
+  Console.WriteLine($"Measuremtn: {result}");
 }
 else
 {
   Console.WriteLine($"Unable to report the measurement.");
 }
 
-Console.WriteLine($"Measurement with offset: {50 + result}");
+if (result > 0)
+  Console.WriteLine($"Measurement with offset: {50 + result}");

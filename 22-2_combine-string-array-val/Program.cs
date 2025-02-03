f17c796f99cb46734dee05f664b365eaad1fae6a
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-04 01:39:30 +0200

    Refactor TryParse usage to declare result inline for improved readability

--- 22-2_combine-string-array-val/Program.cs
+++ 22-2_combine-string-array-val/Program.cs
@@ -1,10 +1,9 @@
 ﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-float result = 0;
 float total = 0;
 
 foreach (string value in values)
 {
-  if (float.TryParse(value, out result))
+  if (float.TryParse(value, out float result))
   {
     total += result;
   }

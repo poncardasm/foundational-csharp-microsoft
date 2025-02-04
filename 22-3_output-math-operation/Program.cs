22502ae5a5c5f6ac6e43aa5798c43321f81c7663
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-04 02:10:00 +0200

    Implement division operations with proper rounding and enhance output clarity

--- 22-3_output-math-operation/Program.cs
+++ 22-3_output-math-operation/Program.cs
@@ -2,12 +2,19 @@
 decimal value2 = 6.2m;
 float value3 = 4.3f;
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 ({value1}) by value2 ({value2}), display the result as an int: {result1}");
 
 // Your code here to set result2
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
 // Your code here to set result3
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+/*
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
+*/

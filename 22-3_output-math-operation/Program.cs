dd442f71de10e7f32cc875312b4423bc36dfae15
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-04 02:17:22 +0200

    Add division operation for value2 and value3, displaying the result as a decimal

--- 22-3_output-math-operation/Program.cs
+++ 22-3_output-math-operation/Program.cs
@@ -5,8 +5,8 @@ float value3 = 4.3f;
 int result1 = Convert.ToInt32(value1 / value2);
 Console.WriteLine($"Divide value1 ({value1}) by value2 ({value2}), display the result as an int: {result1}");
 
-// Your code here to set result2
-// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
 // Your code here to set result3
 // Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");

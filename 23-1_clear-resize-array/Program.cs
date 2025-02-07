453f9f9ac12f80e63966d88c5924b42f56bd7e09
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-08 00:02:36 +0200

    Add project for clearing elements in an array and display results

---
+++ 23-1_clear-resize-array/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] pallets = ["B14", "A11", "B12", "A13"];
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}

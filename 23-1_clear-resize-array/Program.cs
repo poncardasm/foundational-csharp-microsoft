25ec1530b1927a8bf0c99e0a2b495f7052897318
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-08 00:46:20 +0200

    Add functionality to resize pallets array and display contents after clearing

--- 23-1_clear-resize-array/Program.cs
+++ 23-1_clear-resize-array/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-string[] pallets = ["B14", "A11", "B12", "A13"];
+﻿string[] pallets = ["B14", "A11", "B12", "A13"];
 
 Console.WriteLine($"Before: {pallets[0].ToLower()}");
 Array.Clear(pallets, 0, 2);
@@ -11,6 +9,17 @@ if (pallets[0] != null)
 }
 
 Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
 
 foreach (var pallet in pallets)
 {

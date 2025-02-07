c82e1c5a7d13d4dc33f06e85cc27746f45346958
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-08 00:39:25 +0200

    Improve output formatting for array clearing by converting to lowercase

--- 23-1_clear-resize-array/Program.cs
+++ 23-1_clear-resize-array/Program.cs
@@ -2,9 +2,13 @@
 
 string[] pallets = ["B14", "A11", "B12", "A13"];
 
-Console.WriteLine($"Before: {pallets[0]}");
+Console.WriteLine($"Before: {pallets[0].ToLower()}");
 Array.Clear(pallets, 0, 2);
-Console.WriteLine($"After: {pallets[0]}");
+
+if (pallets[0] != null)
+{
+  Console.WriteLine($"After: {pallets[0].ToLower()}");
+}
 
 Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
 

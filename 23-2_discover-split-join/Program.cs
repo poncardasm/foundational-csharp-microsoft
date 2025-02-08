77665d21915fcb2dcd1bab7616fcffd2e465b0fb
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-09 00:19:18 +0200

    Add reverse, join, and split functionality with output display

--- 23-2_discover-split-join/Program.cs
+++ 23-2_discover-split-join/Program.cs
@@ -1,7 +1,23 @@
-﻿string value = "abc123";
+﻿Console.WriteLine("\n--- Reverse ---\n");
+string value = "abc123";
 char[] valueArray = value.ToCharArray();
 
 Array.Reverse(valueArray);
 
 string reversedValue = new string(valueArray);
 Console.WriteLine(reversedValue);
+
+Console.WriteLine("\n--- Join ---\n");
+string value2 = "abc123";
+char[] valueArray2 = value2.ToCharArray();
+Array.Reverse(valueArray2);
+
+string reversedValue2 = string.Join(",", valueArray2);
+Console.WriteLine(reversedValue2);
+
+Console.WriteLine("\n--- Split ---\n");
+string[] items = reversedValue2.Split(',');
+foreach (string item in items)
+{
+  Console.WriteLine(item);
+}

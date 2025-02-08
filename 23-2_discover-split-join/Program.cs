1285f04efad3e5fbad094b6a2089a94f9e798548
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-08 22:52:47 +0200

    Add project for discovering split and join functionality with string reversal

---
+++ 23-2_discover-split-join/Program.cs
@@ -0,0 +1,7 @@
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+
+Array.Reverse(valueArray);
+
+string reversedValue = new string(valueArray);
+Console.WriteLine(reversedValue);

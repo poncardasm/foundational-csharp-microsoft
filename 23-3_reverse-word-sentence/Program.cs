61b5cb00305c80a9ac54474b22a33480e1a1dd3e
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-09 01:48:36 +0200

    Add project for reversing words in a sentence with example implementation

---
+++ 23-3_reverse-word-sentence/Program.cs
@@ -0,0 +1,13 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] pangramSplits = pangram.Split(" ");
+
+foreach (var pangramSplit in pangramSplits)
+{
+  char[] pangramChars = pangramSplit.ToArray();
+  Array.Reverse(pangramChars);
+
+  string pangramReverse = new(pangramChars);
+
+  Console.Write(pangramReverse + " ");
+}
+

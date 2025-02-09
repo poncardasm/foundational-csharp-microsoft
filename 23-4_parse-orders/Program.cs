29494d5f74febcd1964dc028546950035c2aa641
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-09 15:11:15 +0200

    Add project for parsing and validating order strings

---
+++ 23-4_parse-orders/Program.cs
@@ -0,0 +1,16 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderLists = orderStream.Split(',');
+Array.Sort(orderLists);
+
+foreach (var orderList in orderLists)
+{
+  if (orderList.Length != 4)
+  {
+    Console.WriteLine($"{orderList} \t - Error");
+  }
+  else
+  {
+    Console.WriteLine(orderList);
+  }
+}

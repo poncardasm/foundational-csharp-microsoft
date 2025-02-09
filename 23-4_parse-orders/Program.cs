9dd3a64bfbc4f2c9e7dd32d234571ad63fbfe159
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-09 15:15:39 +0200

    Add commented solution for parsing and validating order strings

--- 23-4_parse-orders/Program.cs
+++ 23-4_parse-orders/Program.cs
@@ -14,3 +14,24 @@ foreach (var orderList in orderLists)
     Console.WriteLine(orderList);
   }
 }
+
+/*
+ * Given solution
+ */
+
+//  string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] items = orderStream.Split(',');
+// Array.Sort(items);
+
+// foreach (var item in items)
+// {
+//     if (item.Length == 4)
+//     {
+//         Console.WriteLine(item);
+//     }
+//     else
+//     {
+//         Console.WriteLine(item + "\t- Error");
+//     }
+// }
+

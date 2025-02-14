72ccd8a2eff5e5acd5cf027dd59dc46e29aa938d
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-14 23:27:51 +0200

    Add examples of string interpolation in variable data project

--- 24-0_work-with-variable-data/Program.cs
+++ 24-0_work-with-variable-data/Program.cs
@@ -6,3 +6,13 @@ string first = "Hello";
 string second = "World";
 string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result);
+
+/*
+ * String interpolation
+ */
+
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+

717fb47558bdaba8df0b840dd96b8ff2bf41886c
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-14 23:24:38 +0200

    Add composite formatting example in variable data project

--- 24-0_work-with-variable-data/Program.cs
+++ 24-0_work-with-variable-data/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Composite formatting
+ */
+
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);

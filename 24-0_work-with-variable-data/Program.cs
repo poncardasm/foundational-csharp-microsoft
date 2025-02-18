e161905f51e323bff757badba7c2d631e6825b9a
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-19 01:14:19 +0200

    Add examples for string interpolation and parenthesis pair extraction in variable data project

--- 24-0_work-with-variable-data/Program.cs
+++ 24-0_work-with-variable-data/Program.cs
@@ -65,3 +65,20 @@ decimal salePrice2 = 59.99m;
 
 yourDiscount += $"A discount of {((price3 - salePrice2) / price3):P2}!"; //inserted
 Console.WriteLine(yourDiscount);
+
+/*
+ * Exercise - Explore string interpolation
+ */
+Console.WriteLine($"\n-- Exercise - Explore string interpolation --\n");
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
---
+++ 25-0_modify-content-of-strings/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Write code to find parenthesis pairs embedded in a string
+ */
+Console.WriteLine($"\n-- Write code to find parenthesis pairs embedded in a string --\n");
+
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
+/*
+ * Add code to retrieve the value between parenthesis
+ */
+Console.WriteLine($"\n-- Add code to retrieve the value between parenthesis --\n");
+

77d0982c6529df8710423b9f7acb51293d489aec
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-23 23:08:01 +0200

    Add examples for string manipulation using Remove and Replace methods

--- 25-0_modify-content-of-strings/remove-replace-method.cs
+++ 25-0_modify-content-of-strings/remove-replace-method.cs
@@ -7,9 +7,16 @@ public class RemoveReplaceMethod
 {
   public static void Run()
   {
+    // Remove characters in specific locations from a string
     string data = "12345John Smith          5000  3  ";
     string updatedData = data.Remove(5, 20);
     Console.WriteLine(updatedData);
+
+    // Use the Replace() method
+    string message = "This--is--ex-amp-le--da-ta";
+    message = message.Replace("--", " ");
+    message = message.Replace("-", "");
+    Console.WriteLine(message);
   }
 }
 

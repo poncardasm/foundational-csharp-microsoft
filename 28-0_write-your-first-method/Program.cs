c02263dadde2f16818416fc0705f45e10610a590
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-03-16 20:58:48 +0200

    Implement medication schedule adjustment based on time zone input

--- 28-0_write-your-first-method/Program.cs
+++ 28-0_write-your-first-method/Program.cs
@@ -1,22 +1,72 @@
-﻿int[] a = { 1, 2, 3, 4, 5 };
+﻿/*
+  Exercise - Create reusable methods
 
-Console.WriteLine($"Contents of Array: ");
-printArray();
+  In this task, you'll take a look at an application that tracks medication times across different time zones. The user enters their current time zone and their destination time zone. Their medication schedule is displayed and then adjusted for the new time zone.
+*/
 
-void printArray()
+int[] times = [800, 1200, 1600, 2000];
+int diff = 0;
+
+Console.WriteLine("Enter current GMT");
+int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Current Medicine Schedule:");
+DisplayTimes();
+
+
+Console.WriteLine("Enter new GMT");
+int newGMT = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 {
-  foreach (int x in a)
-  {
-    Console.WriteLine($"{x}");
-  }
-  Console.WriteLine($"");
+  Console.WriteLine("Invalid GMT");
+}
+else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+{
+  diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+  AdjustTimes();
+}
+else
+{
+  diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+  AdjustTimes();
 }
 
-Console.WriteLine($"Before calling a method");
-SayHello();
-Console.WriteLine($"After calling a method");
+Console.WriteLine("New Medicine Schedule:");
+DisplayTimes();
 
-void SayHello()
+void DisplayTimes()
 {
-  Console.WriteLine($"Hello world!");
+  /* Format and display medicine times */
+  foreach (int val in times)
+  {
+    string time = val.ToString();
+    int len = time.Length;
+
+    if (len >= 3)
+    {
+      time = time.Insert(len - 2, ":");
+    }
+    else if (len == 2)
+    {
+      time = time.Insert(0, "0:");
+    }
+    else
+    {
+      time = time.Insert(0, "0:0");
+    }
+
+    Console.Write($"{time} ");
+  }
+
+  Console.WriteLine();
+}
+
+void AdjustTimes()
+{
+  // Adjust the times by adding the difference, keeping the value within 24 hours
+  for (int i = 0; i < times.Length; i++)
+  {
+    times[i] = (times[i] + diff) % 2400;
+  }
 }

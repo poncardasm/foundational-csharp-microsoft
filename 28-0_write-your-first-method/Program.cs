d9df763334c9bd691797c25b45b3cc6cdb48727f
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-03-16 16:54:18 +0200

    Add array printing and method demonstration in Program.cs

--- 28-0_write-your-first-method/Program.cs
+++ 28-0_write-your-first-method/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int[] a = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine($"Contents of Array: ");
+printArray();
+
+void printArray()
+{
+  foreach (int x in a)
+  {
+    Console.WriteLine($"{x}");
+  }
+  Console.WriteLine($"");
+}
+
+Console.WriteLine($"Before calling a method");
+SayHello();
+Console.WriteLine($"After calling a method");
+
+void SayHello()
+{
+  Console.WriteLine($"Hello world!");
+}
